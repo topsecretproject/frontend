7abe36835a7aa383fb7363425aa83c4f76fbe50c
giannagalard <giannagalard@gmail.com>
2022-04-30 15:14:13 -0400

    FEAT: Finished Juniors

--- src/pages/Juniors.js
+++ src/pages/Juniors.js
@@ -3,21 +3,8 @@ import { doc, getDoc } from "@firebase/firestore";
 import { useState, useEffect } from "react";
 import db from "../firebase/firebase";
 import Header from "../components/Header";
-import {
-  Button,
-  Card,
-  CardContent,
-  CardActions,
-  Avatar,
-  Grid,
-  Chip,
-  Typography,
-  CardMedia,
-  styled,
-  Paper,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import { borderColor } from "@mui/system";
+import { Grid } from "@mui/material";
+import StudentsCard from "../components/StudentsCard";
 
 export default function Juniors() {
   const [juniors, setJuniors] = useState([]);
@@ -31,10 +18,6 @@ export default function Juniors() {
     setLoading(false);
   };
 
-  const ListItem = styled("li")(({ theme }) => ({
-    margin: theme.spacing(0.5),
-  }));
-
   useEffect(() => {
     fetchData();
   }, []);
@@ -42,54 +25,9 @@ export default function Juniors() {
   return (
     <Fragment>
       <Header />
-      <Grid container spacing={2} padding={2}>
+      <Grid sx={{ mt: 1 }} container spacing={7} padding={5}>
         {juniors.students?.map((student) => (
-          <Grid item xs={12} sm={12} md={4} lg={4} key={student.id}>
-            <CardMedia
-              component="img"
-              height="100"
-              image={student.imgLink}
-              alt="avatar"
-            />
-            <Card variant="outlined">
-              <CardContent>
-                <Typography gutterBottom variant="h5" align="center">
-                  {student.name}
-                  <hr></hr>
-                </Typography>
-                {student.interests?.map((student) => (
-                  <Grid item key={student.interests}>
-                    <Paper
-                      elevation={0}
-                      sx={{
-                        display: "flex",
-                        flexWrap: "wrap",
-                        listStyle: "none",
-                        p: 0.5,
-                        m: 0,
-                        backgroundColor: "#E6E6E6E6",
-                      }}
-                      component="ul"
-                    >
-                      <ListItem key={student}>
-                        <Chip
-                          variant="outlined"
-                          color="info"
-                          size="small"
-                          label={student}
-                        />
-                      </ListItem>
-                    </Paper>
-                  </Grid>
-                ))}
-              </CardContent>
-              <CardActions>
-                <Button variant="contained" component={Link} to="/student">
-                  View Profile
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
+          <StudentsCard student={student} />
         ))}
       </Grid>
     </Fragment>
